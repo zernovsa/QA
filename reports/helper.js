413fd33a106ab6277e502d91f38b14e87f75bc36
TULA\s.zernov <s.zernov@uiscom.ru>
2017-09-26 15:02:57 +0300

    no message

---
+++ reports/helper.js
@@ -0,0 +1,8 @@
+// helpers.js
+export async function getClick (t) {
+    await t.click('.new-btn');
+}
+
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
