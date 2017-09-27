310fa87fcb275a9163b96f40c8e1b61c41d785a1
TULA\s.zernov <s.zernov@uiscom.ru>
2017-09-27 16:03:44 +0300

    no message

--- tests/login.js
+++ tests/login.js
@@ -1,12 +1,14 @@
 import {t, Selector, ClientFunction} from 'testcafe';
 import * as Helper from '../helper.js';
-import {test_link} from '../config.js';
+import {test_link, username, password} from '../config.js';
 
 fixture `Getting Started`
     .page(test_link);
 
 test('login', async () => {
         await t.setTestSpeed(1);
-        await Helper.login();
-    }
+        await t
+	        .typeText('input[name="login"]', username)
+	        .typeText('input[name="password"]', password)
+	        .pressKey('enter')    }
 );
